7058145adb02b393418e07e79d5db40f59020493
Alex Rosca <a.rosca@getyourway.be>
2025-02-19 16:17:17 +0100

    Bigger buffer

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,54 @@
 #include <lvgl.h>
 #include "peripherals/lvgl_driver.h"
 
+lv_obj_t *label;
+bool toggle = true;
+
+// Fonction pour afficher "Hello World!"
+void display_hello_world() {
+    label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Hello World!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_color(label, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_40, LV_PART_MAIN);
+    //
+    // label = lv_label_create(lv_scr_act());
+    // lv_label_set_text(label, "Hello World!");
+    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 200);
+    // lv_obj_set_style_text_color(label, lv_color_make(0xee, 0xee, 0xee), LV_PART_MAIN);
+    // lv_obj_set_style_text_font(label, &lv_font_montserrat_40, LV_PART_MAIN);
+    //
+    // label = lv_label_create(lv_scr_act());
+    // lv_label_set_text(label, "Hello World!");
+    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 300);
+    // lv_obj_set_style_text_color(label, lv_color_white(), LV_PART_MAIN);
+    // lv_obj_set_style_text_font(label, &lv_font_montserrat_40, LV_PART_MAIN);
+}
+
 void setup() {
     Serial.begin(115200);
-
-    // Initialize LVGL
     lv_init();
-
-    // Initialize display driver
     lvgl_display_init();
 
-    // Create a simple UI
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "Hello, World!");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, -20);
-
-    lv_obj_t *rect = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(rect, 100, 50);
-    lv_obj_align(rect, LV_ALIGN_CENTER, 0, 30);
-    lv_obj_set_style_bg_color(rect, lv_color_black(), LV_PART_MAIN);
+    display_hello_world();
 }
 
 void loop() {
-    lv_timer_handler();
-    delay(5);
+    // Gestion des changements de texte pour forcer le flush
+    // static unsigned long previousUpdateTime = 0;
+    // unsigned long currentTime = millis();
+    //
+    // if (currentTime - previousUpdateTime >= 1000) {
+    //     if (toggle) {
+    //         lv_label_set_text(label, "Hello World!");
+    //     } else {
+    //         lv_label_set_text(label, "Bonjour!");
+    //     }
+    //     toggle = !toggle;
+    //     previousUpdateTime = currentTime;
+    // }
+
+    // Appelle explicitement la gestion des tâches LVGL
+    lv_timer_handler(); // Traite les événements et les changements
+    delay(5);  // Pause courte pour éviter la surcharge CPU
 }
